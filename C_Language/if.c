cbb910bbfea0e79125864d1505f3d494f28e1030
darshan <darshan.patel.tops@gmail.com>
2023-06-30 11:51:02 +0530

    first

---
+++ C_Language/if.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+int main()
+{
+	
+	/*
+	
+	if(condtion)
+	{
+		function/statement
+	}
+	else
+	{
+	statement function
+}
+	
+	
+	*/
+	int bhajiya =1;
+	int chai =1;
+	int chutney=1;
+	int mummy;
+	if(mummy)
+	{
+		printf("bha%%jiya");
+	}
+	else
+	{
+		printf("No Bhajiya for you");
+	}
+	
+}
