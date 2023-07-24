42891469fb60fadf926ac7f4c8de88538ef8918d
darshan <darshan.patel.tops@gmail.com>
2023-07-24 10:07:04 +0530

    first

---
+++ Cpp_Language/Untitled1.cpp
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	int percentage;
+	printf("student number %d enter your total marks\n ",i+1);
+	scanf("%f",&percentage);
+	percentage[i]=all student total marks/700*100;
+	switch (percentage)
+	{
+		
+	}
+}
